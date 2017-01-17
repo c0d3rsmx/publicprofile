7bbc4080722ac1d024c90f2309ef2b16738e86f7
Antonio BG <jabg1992@gmail.com>
2017-01-17 14:02:34 -0800

    Added affix menu
    added missing js resources to main layout

--- src/Providers/PublicProfileServiceProvider.php
+++ src/Providers/PublicProfileServiceProvider.php
@@ -20,7 +20,8 @@ class PublicProfileServiceProvider extends ServiceProvider
             __DIR__.'/../routes' => base_path('routes'),
             __DIR__.'/../Controllers' => base_path('app/Http/Controllers'),
         ]);
-        $this->loadViewsFrom(base_path('resources/views/so2platform/publicprofile'), 'publicprofile');
+        $this->loadViewsFrom(__DIR__.'/../views', 'publicprofile');
+//        $this->loadViewsFrom(base_path('resources/views/so2platform/publicprofile'), 'publicprofile');
         if(file_exists(base_path('routes/publicprofile.php'))) {
             $this->loadRoutesFrom(base_path('routes/publicprofile.php'));
         }
--- src/views/frontend/layout/app.blade.php
+++ src/views/frontend/layout/app.blade.php
@@ -9,6 +9,7 @@
     <link href="https://so2cloud.s3.amazonaws.com/public/assets/synergyo2corp/bootstrap_3/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
     <link href="https://so2cloud.s3.amazonaws.com/public/assets/synergyo2corp/bootstrap_3/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" media="all"/>
     <script src="https://code.jquery.com/jquery-3.1.0.js" integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk="   crossorigin="anonymous"></script>
+    <script src="https://so2cloud.s3.amazonaws.com/public/assets/synergyo2corp/bootstrap_3/js/bootstrap.min.js" type="text/javascript"></script>
     <script src="https://so2cloud.s3.amazonaws.com/public/assets/vendor/vuejs/vue.1_0_22.min.js" type="text/javascript"></script>
     <script src="https://so2cloud.s3.amazonaws.com/public/assets/vendor/vuejs/vue-resource.min.js" type="text/javascript"></script>
     <script src="https://npmcdn.com/vue-select@latest"></script>
